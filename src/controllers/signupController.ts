f5eaf2febb7324ac15057abd56b3516a5d0af6f1
yueayase <racham.chen@gmail.com>
2024-08-12 18:54:24 +0800

    change the database structure for storing users and change signupController so that we can use the new database structure

--- src/controllers/signupController.ts
+++ src/controllers/signupController.ts
@@ -4,26 +4,33 @@ import { Knex } from "knex";
 export const mountSignupRouter = (knexSql: Knex) => {
     const signupController = async (req: Request, res: Response) => {
         const { name, EmailOrCellPhoneNumber, password, birthday, gender, genderAlias } = req.body;
-        console.log("test...");
-        console.log(name);
-        console.log(EmailOrCellPhoneNumber);
-        console.log(password);
-        console.log(birthday);
-        console.log(gender);
-        console.log(genderAlias);
         try {
             const isCellPhoneNumber = /^(\+\d{1,3}[- ]?)?\d{10}$/.test(EmailOrCellPhoneNumber);
            
             const trx = await knexSql.transaction(async (trx: Knex.Transaction) => {
                 await trx("User").insert({ 
                     name: name, 
-                    email: isCellPhoneNumber ? null : EmailOrCellPhoneNumber, 
-                    cellPhoneNumber: isCellPhoneNumber ? EmailOrCellPhoneNumber : null,
                     password: password,
                     birthday: birthday,
                     gender: gender,
                     genderAlias: genderAlias  
-                });
+                }).then(
+                    async(row) => {
+                        const userId = row[0];  // See https://stackoverflow.com/questions/43465266/knex-does-not-return-insert-id
+                        if (isCellPhoneNumber){
+                            await trx("UserCellPhone").insert({
+                                userId: userId,
+                                cellPhoneNumber: EmailOrCellPhoneNumber
+                            });
+                        }else{
+                            await trx("UserEmail").insert({
+                                userId: userId,
+                                email: EmailOrCellPhoneNumber
+                            });
+                        }
+                        //console.log(row);
+                    }
+                );
             });
 
             res.status(200).json({ message: `successfully insert user ${name}`});
--- src/database/create_user_table.sql
+++ src/database/create_user_table.sql
@@ -1,12 +1,19 @@
-CREATE TABLE `User`(
-	`id` SERIAL PRIMARY KEY,
+USE MyFacebookDatabase;
+CREATE TABLE `User` (
+    `id` SERIAL PRIMARY KEY,
     `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) UNIQUE,
-    `cellPhoneNumber` VARCHAR(20) UNIQUE,
     `password` VARCHAR(40) NOT NULL,
     `birthday` DATE NOT NULL,
     `gender` VARCHAR(100) NOT NULL,
-    `genderAlias` VARCHAR(10) NOT NULL,
-    
-    CONSTRAINT `id_must_have` CHECK (`email` iS NOT NULL OR `cellPhoneNumber` is NOT NULL)
+    `genderAlias` VARCHAR(10) NOT NULL
+);
+CREATE TABLE `UserEmail` (
+    `userId` BIGINT UNSIGNED PRIMARY KEY,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE
+);
+CREATE TABLE `UserCellPhone` (
+    `userId` BIGINT UNSIGNED PRIMARY KEY,
+    `cellPhoneNumber` VARCHAR(20) UNIQUE NOT NULL,
+    FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE
 );
